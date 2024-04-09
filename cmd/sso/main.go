479415d9251ae2710841025daf1d8dbb3c553d35
satori <emogycs@gmail.com>
2024-04-09 21:37:41 +0300

    added

---
+++ cmd/sso/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"gRPC_auth/internal/app"
+	"gRPC_auth/internal/config"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.String("env", cfg.Env))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+
+	log.Info("stopping application", slog.String("signal", sign.String()))
+
+	application.GRPCSrv.Stop()
+
+	log.Info("application stopped")
+
+	// TODO: запустить gRPC сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,122 @@
+package auth
+
+import (
+	"context"
+	"gRPC_auth/protos/gen/go/sso"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Auth interface {
+	Login(ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+
+	RegisterNewUser(ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+const (
+	emptyID    = 0
+	emptyValue = ""
+)
+
+type serverAPI struct {
+	sso.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(gRPC *grpc.Server, auth Auth) {
+	sso.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *sso.LoginRequest) (*sso.LoginResponse, error) {
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &sso.LoginResponse{
+		Token: token,
+	}, nil
+}
+
+func (s *serverAPI) Register(ctx context.Context, req *sso.RegisterRequest) (*sso.RegisterResponse, error) {
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &sso.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *sso.IsAdminRequest) (*sso.IsAdminResponse, error) {
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &sso.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+func validateLogin(req *sso.LoginRequest) error {
+	if req.GetEmail() == emptyValue {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == emptyValue {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyID {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *sso.RegisterRequest) error {
+	if req.GetEmail() == emptyValue {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == emptyValue {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func validateIsAdmin(req *sso.IsAdminRequest) error {
+	if req.GetUserId() == emptyID {
+		return status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	return nil
+}
